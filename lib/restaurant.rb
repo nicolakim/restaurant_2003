56d9e62db39efebcbef24903329e88c4639af3ce
Nicola Kim <nicola.copsey@gmail.com>
2020-03-18 12:53:17 -0700

    Initial commit

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,10 @@
+class Restaurant
+  attr_reader :name,
+              :opening_time
 
+  def restaurant_details(opening_time, name)
+    @name = restaurant_name
+    @opening_time = open_time
+  end
+
+end
